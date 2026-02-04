aecf74bb58975d110f96d92e31f9ac1eb5409495
bangjago111004-collab <bangjago111004@gmail.com>
2026-02-04 12:13:57 +0700

    deploy via github actions

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/publisherldp">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
